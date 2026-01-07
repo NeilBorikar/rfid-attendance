963ac80e87c0996e41228015a629941948d9d52d
Neil Borikar <neilborikar25@gmail.com>
2026-01-08 00:12:26 +0530

    fixed pydantic issue

--- app/settings.py
+++ app/settings.py
@@ -1,13 +1,16 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 
-class AppSettings(BaseSettings):
-    APP_NAME: str = "RFID Attendance Backend"
-    APP_ENV: str = "development"
-    DEBUG: bool = True
+class Settings(BaseSettings):
+    MONGO_URI: str
+    MONGO_DB_NAME: str
+
+    TWILIO_ACCOUNT_SID: str
+    TWILIO_AUTH_TOKEN: str
+    TWILIO_WHATSAPP_FROM: str
 
     class Config:
         env_file = ".env"
 
 
-settings = AppSettings()
+settings = Settings()
