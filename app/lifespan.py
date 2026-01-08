2868ca26d67a9533375a868e3387a486b1cd8ecd
Neil Borikar <neilborikar25@gmail.com>
2026-01-08 18:29:06 +0530

    fixing deployment issues

--- app/lifespan.py
+++ app/lifespan.py
@@ -1,17 +1,19 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
-from core.database import Database
+from core.database import MongoDB
+
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # 🔹 Startup
-    Database.connect()
+    MongoDB.connect()
+
     print("✅ Database connected")
 
     yield
 
     # 🔹 Shutdown
-    Database.close()
+    MongoDB.close()
     print("🛑 Database connection closed")
